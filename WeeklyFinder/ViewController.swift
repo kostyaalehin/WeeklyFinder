ebf84c05247fef9b0b9016080b432403c76f34a0
kostyaalehin <koss595959@gmail.com>
2022-04-20 23:12:38 +0500

    Добавил swiftlint

--- WeeklyFinder/ViewController.swift
+++ WeeklyFinder/ViewController.swift
@@ -13,33 +13,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var monthTextField: UITextField!
     @IBOutlet weak var yearTextField: UITextField!
     @IBOutlet weak var dayLabel: UILabel!
-   
+
     @IBAction func seachAction(_ sender: UIButton) {
-        
+
         guard let day = dayTextField.text, !day.isEmpty,
-                let month = monthTextField.text, 
+                let month = monthTextField.text,
                 let year = yearTextField.text else {
             dayLabel.text = "Ошибка"
             return
-            
+
         }
-        
+
         let calendar = Calendar.current
         var dateComponents = DateComponents()
         dateComponents.day = Int(day)
         dateComponents.month = Int(month)
         dateComponents.year = Int(year)
-        
+
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "EEEE"
         dateFormatter.locale = Locale(identifier: "ru_Ru")
-        
+
         guard let date = calendar.date(from: dateComponents) else {
             dayLabel.text = "Ошибка 2"
             return
-            
         }
-        
+
         let weekday = dateFormatter.string(from: date)
         let capitalizedWeekDay = weekday.capitalized
         dayLabel.text = capitalizedWeekDay
@@ -47,8 +46,5 @@ class ViewController: UIViewController {
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
-        
     }
 }
-
-
