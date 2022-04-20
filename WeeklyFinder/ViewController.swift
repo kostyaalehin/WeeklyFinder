e4a12f29f84d2526b9445bce3f83ea72279f12dc
kostyaalehin <koss595959@gmail.com>
2022-04-20 22:28:50 +0500

    Сделал начальный экран

--- WeeklyFinder/ViewController.swift
+++ WeeklyFinder/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var dayTextField: UITextField!
+    @IBOutlet weak var monthTextField: UITextField!
+    @IBOutlet weak var yearTextField: UITextField!
+    @IBOutlet weak var dayLabel: UILabel!
+   
+    @IBAction func seachAction(_ sender: UIButton) {
+        
+        guard let day = dayTextField.text, !day.isEmpty,
+                let month = monthTextField.text, 
+                let year = yearTextField.text else {
+            dayLabel.text = "Ошибка"
+            return
+            
+        }
+        
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        
+        guard let date = calendar.date(from: dateComponents) else {
+            dayLabel.text = "Ошибка 2"
+            return
+            
+        }
+        
+        let weekday = dateFormatter.string(from: date)
+        let capitalizedWeekDay = weekday.capitalized
+        dayLabel.text = capitalizedWeekDay
     }
 
-
 }
+
 
